da0c10232cd6c5695edb0b15d9e8ea1892b51062
ShambleShef <matt_starin754@hotmail.com>
2017-12-08 14:03:52 -0800

    add template html code and initial js business logic

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// BUSINESS END //
+(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var q1 = $("#q1").val();
+    var q2 = $("#q2").val();
+    var q3 = $("#q3").val();
+  };
+
+
+
+
+// USER INTERFACE //
